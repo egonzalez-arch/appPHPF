8575965d6416e9ef7f6179280b8d7d1ce9214fbb
Eddiee Even <egonzalez@grupopf.com.mx>
2025-09-12 10:25:01 -0600

    css comentado

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { AuthProvider } from "@/context/AuthContext";
-import "../styles/globals.css"; // o el path real de tu CSS global
+//import "../styles/globals.css"; // o el path real de tu CSS global
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
